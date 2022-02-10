42ce0875e868bcff3519026f1841477f9ebf7af5
unknown <erh104@pitt.edu>
2022-02-09 19:23:20 -0500

    second commit

---
+++ App.java
@@ -0,0 +1,13 @@
+/**
+ * A very very basic driver for CS1501 Project 1
+ * @author	Dr. Farnan
+ */
+package cs1501_p1;
+
+public class App {
+	public static void main(String[] args) {
+		BST tree = new BST();
+		System.out.println("Successfully built an empty tree!");
+		
+	}
+}
